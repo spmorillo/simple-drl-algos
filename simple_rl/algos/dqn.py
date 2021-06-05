860cc779f64603494c7f2fa2cadeba31aa250ea8
spmorillo <sergipm11@hotmail.com>
2021-06-05 17:22:31 +0200

    add first project structure, update gitigore

---
+++ simple_rl/algos/dqn.py
@@ -0,0 +1,6 @@
+# logger (tensorboardx) O, docs (sphynx) X, linter (flake8) X,
+# auto-formatter (blake) X, tests (unittest, github actions) O,
+# docker O, env (conda) X, debug (vscode) X, control (git) X
+
+import torch
+import gym
